67e5d4e414e324fdacc8cbc8331afd25a046cdbc
andvea <36574883+andvea@users.noreply.github.com>
2026-04-11 14:23:41 +0200

    Add signin and signup with mysql

---
+++ classes/Customer.js
@@ -0,0 +1,130 @@
+export class Customer {
+  static hashPwd(pwd){
+    return pwd;
+  }
+
+  constructor(id) {
+    this.id = id;
+  }
+
+  setId(id){
+    this.id = id;
+  }
+
+  getId(){
+    return this.id;
+  }
+  
+  getEmail(){
+    return this.email;
+  }
+
+  setEmail(email){
+    this.email = email;
+  }
+
+  getPwd(){
+    return this.pwd;
+  }
+
+  setPwd(pwd){
+    this.pwd = pwd;
+  }
+
+  getFirstName(){
+    return this.first_name;
+  }
+
+  setFirstName(firstName){
+    this.first_name = firstName;
+  }
+
+  getLastName(){
+    return this.last_name;
+  }
+
+  setLastName(lastName){
+    this.last_name = lastName;
+  }
+
+  getProfilePic(){
+    return this.profile_pic;
+  }
+
+  setProfilePic(profilePic){
+    this.profile_pic = profilePic;
+  }
+}
+
+export class CustomerRepository {
+	constructor(mySqlPool) {
+    this.mySqlPool = mySqlPool;
+  }
+
+  async find(email = null, pwd = null) {
+    try {
+      var customerFromDb = await this.mySqlPool.query(`
+        SELECT * 
+        FROM ${global.ENV.DATABASE_CUSTOMERS_TABLE} 
+        WHERE email = ? AND pwd = ?
+        LIMIT 1`,
+        [email, pwd]);
+
+      if (customerFromDb[0].length==1) {
+        var c = new Customer(customerFromDb[0][0].local_id);
+        c.setEmail(customerFromDb[0][0].email);
+        c.setFirstName(customerFromDb[0][0].first_name);
+        c.setLastName(customerFromDb[0][0].last_name);
+        c.setProfilePic(customerFromDb[0][0].profile_pic);
+
+        return c;
+      } else {
+        return null;
+      }
+
+    } catch(dbError) {
+      throw new Error('Can\'t find customer', {
+        cause: {
+          errorCode: 500, 
+          originError: dbError
+        }
+      });
+    }
+  }
+
+  async create(customer) {
+    try {
+      var res = await this.mySqlPool.query(`
+        INSERT INTO 
+          ${global.ENV.DATABASE_CUSTOMERS_TABLE}
+          (email, pwd, first_name, last_name, profile_pic)
+        VALUES (?, ?, ?, ?, ?)
+        ON DUPLICATE KEY UPDATE email = email`,
+        [
+          customer.getEmail(),
+          customer.getPwd(),
+          customer.getFirstName(), 
+          customer.getLastName(),
+          customer.getProfilePic()
+        ]);
+
+      if (!res[0].insertId) {
+        throw new Error('Customer already exists', {
+          cause:{ errorCode: 409, originError: null }
+        });
+      }
+
+      customer.setId(res[0].insertId);
+
+      return customer;
+    } catch(dbError) {
+      throw new Error((dbError.message ? dbError.message : 'An error occurred with db'), {
+        cause: {
+          errorCode: (dbError.cause.errorCode ? dbError.cause.errorCode : 500),
+          originError: dbError
+        }
+      });
+    }
+  }
+
+}
--- server.js
+++ server.js
@@ -3,12 +3,30 @@
 import express from 'express';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import * as dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
 
 import {Authentication} from './classes/Authentication.js';
+import {Customer, CustomerRepository} from './classes/Customer.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+
+dotenv.config({ path: './.env.dev' });
+global.ENV = process.env;
+
+var mysqlPool = mysql.createPool({
+  connectionLimit : 100,
+  user            : global.ENV.DATABASE_USER,
+  password        : global.ENV.DATABASE_PASSWORD,
+  database        : global.ENV.DATABASE_DATABASE,
+  host            : global.ENV.DATABASE_HOST,
+  port            : global.ENV.DATABASE_PORT
+});
+
+const customerRepository = new CustomerRepository(mysqlPool);
+const authentication = new Authentication();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({
@@ -26,8 +44,6 @@ app.use((req, res, next) => {
   next();
 });
 app.use('/static', express.static('/static'));
-
-const authentication = new Authentication();
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, 'localhost');
@@ -47,7 +63,7 @@ app.use(function (req, res, next){
     return res.redirect('/admin/home');
   }
 
-  if (!(req.path == '/signin' && req.method == 'GET') && !req.AUTH_MIDDLEWARE.userInfo) {
+  if (req.path != '/signin' && req.path != '/signup' && !req.AUTH_MIDDLEWARE.userInfo) {
     return res.redirect('/signin?redirectTo='+req.path);
   }
 
@@ -62,20 +78,67 @@ app.get('/signin', async function(req, res){
   res.sendFile(__dirname+'/views/signin.html');
 });
 
+app.get('/signup', async function(req, res){
+  res.sendFile(__dirname+'/views/signup.html');
+});
+
 app.post('/signin', async function(req, res){
-  if (req.query.test=='1') {
+  const email = req.body.email;
+  const pwd = req.body.pwd;
+
+  if (!email || !pwd){
+    return res.redirect('/signin?errCode=400');
+  }
+
+  try{
+    var customer = await customerRepository.find(email, pwd);
+
+    if (!customer) {
+      return res.redirect('/signin?errCode=401');
+    }
+
     var userInfo = {
-      userId: 1,
-      email: 'pxxxxxx.1xxxxx0@studenti.uniroma1.it',
-      first_name: 'Andrea',
-      last_name: 'P',
-      picture: 'https://placehold.co/32x32/fc4b92/white'
+      userId: customer.getId(),
+      email: customer.getEmail(),
+      first_name: customer.getFirstName(),
+      last_name: customer.getLastName(),
+      picture: customer.getProfilePic()
     }
 
     authentication.setAuthCookie( res, userInfo );
     return res.redirect((req.query.redirectTo ? req.query.redirectTo : '/admin/home'));
-  } else {
+  } catch(err) {
+    let errCode = (err.cause && err.cause.errorCode ? err.cause.errorCode : 500);
+    return res.redirect('/signin?errCode='+errCode);
+  }
+});
+
+app.post('/signup', async function(req, res){
+  const firstName = req.body.first_name;
+  const lastName = req.body.last_name;
+  const profilePic = req.body.profile_pic;
+  const email = req.body.email;
+  const pwd = req.body.pwd;
+
+  if (!email || !pwd){
+    return res.redirect('/signup?errCode=400');
+  }
+
+  try{
+    var customer = new Customer(null);
+
+    customer.setFirstName(firstName);
+    customer.setLastName(lastName);
+    customer.setProfilePic(profilePic);
+    customer.setEmail(email);
+    customer.setPwd(Customer.hashPwd(pwd));
+
+    customer = await customerRepository.create(customer);
+
     return res.redirect('/signin');
+  } catch(err) {
+    let errCode = (err.cause && err.cause.errorCode ? err.cause.errorCode : 500);
+    return res.redirect('/signup?errCode='+errCode);
   }
 });
 
